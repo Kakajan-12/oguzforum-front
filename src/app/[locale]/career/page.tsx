6682caf9f82de79d1011802c934555f41b6ea08b
Kakajan <kakajantayjikov@gmail.com>
2025-05-22 10:43:31 +0500

    ui fix

--- src/app/[locale]/career/page.tsx
+++ src/app/[locale]/career/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 import CareerCardProps from "../../Components/CareerComponents/CareerCardProps";
 import { useApplyJobMutation, useGetCareerQuery } from "@/app/Apis/api";
-import React, { useState } from "react";
+import React, { useState,useRef} from "react";
 import { RiDeleteBin5Line } from "react-icons/ri"
 import { PhoneInput } from 'react-international-phone';
 import 'react-international-phone/style.css';
@@ -11,6 +11,7 @@ import { AnimatePresence, motion } from "motion/react";
 import NavigationBackKnob from "@/app/Components/ForBackKnob/NavigationBackKnob";
 import BackgroundUi from "@/app/BackgroundUI/BackgroundStatic";
 import { useTranslations } from "next-intl";
+import {BASE_API_URL} from "@/constant";
 
 export default function CareerRedirectPage() {
     const t = useTranslations("careers");
@@ -21,25 +22,33 @@ export default function CareerRedirectPage() {
     const [selectedTab, setSelectedTab] = useState<"vacancies" | "apply">("vacancies");
     const [selectedVacancyId, setSelectedVacancyId] = useState<number | null>(null);
 
-    const [firstName, setFirstName] = useState("");
-    const [lastName, setLastName] = useState("");
+    const [name, setName] = useState("");
+    const [surname, setSurname] = useState("");
     const [email, setEmail] = useState("");
-    const [phoneNumber, setPhoneNumber] = useState("");
+    const [number, setNumber] = useState("");
     const [photo, setPhoto] = useState<File | null>(null);
+    const [photoPreview, setPhotoPreview] = useState<string | null>(null);
+    const [address, setAddress] = useState("");
+    const [education, setEducation] = useState("");
+    const [comment, setComment] = useState("");
+    const [experience, setExperience] = useState<File | null>(null);
+    const [cvPreview, setCvPreview] = useState<string | null>(null);
 
-    const handlePhoneChange = (value: any) => setPhoneNumber(value);
     const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const file = e.target.files?.[0];
-        if (file) setPhoto(file);
+        if (file) {
+            setPhoto(file);
+            setPhotoPreview(URL.createObjectURL(file)); // для превью
+        }
     };
 
-    const getResponsiveHeight = () => {
-        if (typeof window !== 'undefined') {
-            if (window.innerWidth < 640) return '34px';
-            if (window.innerWidth < 768) return '34px';
-            if (window.innerWidth < 1025) return '50px';
-        }
-        return '54px';
+
+    const handleFileChangeCv = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const file = e.target.files?.[0];
+        if (file) {
+            setExperience(file);
+            setCvPreview(URL.createObjectURL(file)); // для превью
+        };
     };
 
     const forToggleOpen = (section: string) => {
@@ -51,32 +60,85 @@ export default function CareerRedirectPage() {
         setSelectedTab("apply");
     };
 
-    const handleSubmit = async (e: React.FormEvent) => {
+    const photoInputRef = useRef<HTMLInputElement | null>(null);
+    const cvInputRef = useRef<HTMLInputElement | null>(null);
+
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
 
+        if (!photo) {
+            alert("Пожалуйста, загрузите фотографию.");
+            return;
+        }
+
         const formData = new FormData();
-        formData.append('firstName', firstName);
-        formData.append('lastName', lastName);
+        formData.append('name', name);
+        formData.append('surname', surname);
         formData.append('email', email);
-        formData.append('phone', phoneNumber);
-        if (photo) formData.append('photo', photo);
+        formData.append('number', number);
+        formData.append('address', address);
+        formData.append('education', education);
+        formData.append('comment', comment);
+        // formData.append('career_id', selectedVacancyId.toString());
+        formData.append('photo', photo);        // File object
+        // formData.append('experience', experience);      // File object
+
         if (selectedVacancyId !== null) {
-            formData.append('vacancyId', selectedVacancyId.toString());
+            formData.append('career_id', selectedVacancyId.toString());
+        }
+
+        if (experience) {
+            formData.append('experience', experience);
+        } else {
+            const emptyFile = new Blob([""], { type: "text/plain" });
+            formData.append('experience', emptyFile, "empty.txt");
         }
 
         try {
-            const res = await createProfile(formData).unwrap();
-            console.log("Успешно отправлено:", res);
-            // можно сбросить форму или показать сообщение
+            const res = await fetch('https://api.oguzforum.com/api/apply-job', {
+                method: 'POST',
+                body: formData,
+            });
+
+            const data = await res.json();
+            console.log('✅ Ответ от сервера:', data);
+
+            if (!res.ok) {
+                throw new Error(data.error || 'Ошибка при отправке');
+            }
+
         } catch (error) {
-            console.error("Ошибка при отправке:", error);
+            console.error('❌ Ошибка отправки:', error);
         }
     };
+
+
 
     if (isLoading) return <p>loading...</p>;
     if (error) return <p>error...</p>;
     if (!data) return <p>not found...</p>;
 
+    const clearPersonal = () => {
+        setName("");
+        setSurname("");
+        setEmail("");
+        setNumber("");
+        setPhoto(null);
+        setPhotoPreview(null);
+        if (photoInputRef.current) {
+            photoInputRef.current.value = "";
+        }
+    };
+
+    const clearProfile = () => {
+        setEducation("");
+        setComment("");
+        setExperience(null);
+        setCvPreview(null);
+        if (cvInputRef.current) {
+            cvInputRef.current.value = "";
+        }
+    };
     return (
         <>
             <BackgroundUi src="Career.png" name="career"/>
@@ -128,7 +190,7 @@ export default function CareerRedirectPage() {
                                 <div className="flex items-end cursor-pointer">
                                     <RiDeleteBin5Line
                                         className="w-4 h-4 md:w-5 md:h-5 lg:h-7 lg:w-7 mb-[1px] mr-2 text-mainBlue"/>
-                                    <p className="text-mainBlue font-semibold text-xs md:text-sm lg:text-lg xl:text-xl">{t('clear')}</p>
+                                    <div className="text-mainBlue font-semibold text-xs md:text-sm lg:text-lg xl:text-xl" onClick={clearPersonal}>{t('clear')}</div>
                                 </div>
                             </div>
                             <div className="bg-mainBlue mt-2 w-full h-[2px]"></div>
@@ -136,15 +198,15 @@ export default function CareerRedirectPage() {
                             <div className="grid gap-y-5 px-5 md:px-10 mt-7 md:mt-10 gap-x-10 grid-cols-1">
                                 <div className="flex flex-col md:flex-row justify-between gap-5 md:gap-10">
                                     <input
-                                        value={firstName}
-                                        onChange={(e) => setFirstName(e.target.value)}
+                                        value={name}
+                                        onChange={(e) => setName(e.target.value)}
                                         className="border w-full md:text-lg text-xs border-[#002A5F] lg:py-3 md:py-2.5 py-2 px-4 rounded-[10px]"
                                         placeholder={t('fname')}
                                         type="text"
                                     />
                                     <input
-                                        value={lastName}
-                                        onChange={(e) => setLastName(e.target.value)}
+                                        value={surname}
+                                        onChange={(e) => setSurname(e.target.value)}
                                         className="w-full border md:text-lg text-xs border-[#002A5F] lg:py-3 md:py-2.5 py-2 px-4 rounded-[10px]"
                                         placeholder={t('lname')}
                                         type="text"
@@ -159,17 +221,41 @@ export default function CareerRedirectPage() {
                                 />
                                 <PhoneInput
                                     defaultCountry="tm"
-                                    value={phoneNumber}
-                                    onChange={(phone) => setPhoneNumber(phone)}
-                                    inputClassName="border border-[#002A5F] text-xs md:text-lg lg:py-3 md:py-2.5 py-2 px-4 rounded-[10px] w-full"
+                                    value={number}
+                                    onChange={(phone) => setNumber(phone)}
+
+                                />
+
+                                <input
+                                    value={address}
+                                    onChange={(e) => setAddress(e.target.value)}
+                                    className="col-span-full w-full border md:text-lg text-xs border-[#002A5F] lg:py-3 md:py-2.5 py-2 px-4 rounded-[10px]"
+                                    placeholder={t('address')}
+                                    type="text"
                                 />
 
                                 <label
-                                    className="border flex flex-col justify-center items-center gap-y-5 py-14 md:py-20 md:text-sm text-xs border-[#002A5F66] px-3 rounded-[10px] col-span-full">
-                                    <strong className="md:text-4xl text-xl text-mainBlue">{t('uploadPhoto')}</strong>
-                                    <MdOutlineFileUpload className="w-14 text-mainBlue h-14 md:w-32 md:h-32"/>
-                                    <p className="text-mainBlue">{t('uploadText')}</p>
-                                    <input type="file" onChange={handleFileChange} hidden/>
+                                    className="border flex flex-col justify-center items-center gap-y-4 py-10 md:py-15 md:text-sm text-xs border-[#2A5F66] px-3 rounded-xl col-span-full">
+                                    <strong className="md:text-2xl text-lg text-mainBlue">{t('uploadPhoto')}</strong>
+                                    <MdOutlineFileUpload className="w-10 text-mainBlue h-10 md:w-32 md:h-32"/>
+                                    <p className="text-mainBlue text-xs">{t('uploadText')}</p>
+                                    <input ref={photoInputRef} type="file" onChange={handleFileChange} hidden/>
+                                    {photo && (
+                                        <div className="flex flex-col items-center text-mainBlue mt-4 text-sm">
+                                            {/* Отображение превью, если это изображение */}
+                                            {photo.type.startsWith("image/") && photoPreview ? (
+                                                <img
+                                                    src={photoPreview}
+                                                    alt="Preview"
+                                                    className="w-24 h-24 object-cover rounded-md border border-mainBlue"
+                                                />
+                                            ) : null}
+
+                                            {/* Имя файла */}
+                                            <p className="mt-2">{photo.name}</p>
+                                        </div>
+                                    )}
+
                                 </label>
                             </div>
                         </div>
@@ -179,9 +265,9 @@ export default function CareerRedirectPage() {
                             <div className="flex justify-between items-end">
                                 <h3 className="md:text-3xl lg:text-4xl text-2xl text-mainBlue font-semibold">{t('profile')}</h3>
                                 <div className="flex items-center cursor-pointer">
-                                    <RiDeleteBin5Line
+                                <RiDeleteBin5Line
                                         className="w-4 h-4 md:w-5 md:h-5 lg:h-7 lg:w-7 mb-[1px] mr-2 text-mainBlue"/>
-                                    <p className="text-mainBlue font-semibold text-xs md:text-sm lg:text-lg xl:text-xl">{t('clear')}</p>
+                                    <div className="text-mainBlue font-semibold text-xs md:text-sm lg:text-lg xl:text-xl" onClick={clearProfile}>{t('clear')}</div>
                                 </div>
                             </div>
                             <div className="bg-mainBlue md:mt-5 w-[97%] self-center h-[2px] mt-2"></div>
@@ -200,7 +286,9 @@ export default function CareerRedirectPage() {
                                                         exit={{height: 0}}>
                                                 <div className="py-10">
                                                     <textarea
-                                                        className="w-full border-[#002A5F66] px-4 py-3 rounded-[10px] h-52 resize-none border"/>
+                                                        value={education}
+                                                        className="w-full border-[#2A5F66] px-4 py-3 rounded-xl h-52 resize-none border"
+                                                        onChange={(e) => setEducation(e.target.value)}/>
                                                 </div>
                                             </motion.div>
                                         )}
@@ -219,16 +307,33 @@ export default function CareerRedirectPage() {
                                                         exit={{height: 0}}>
                                                 <div className="py-10 flex flex-col gap-10">
                                                     <label
-                                                        className="border flex flex-col justify-center items-center gap-y-5 py-14 md:py-20 md:text-sm text-xs border-[#002A5F66] px-3 rounded-[10px] col-span-full">
+                                                        className="border flex flex-col justify-center items-center gap-y-5 py-10 md:py-12 md:text-sm text-xs border-[#2A5F66] px-3 rounded-md col-span-full">
                                                         <strong
-                                                            className="md:text-4xl text-xl text-mainBlue">{t('uploadCV')}</strong>
+                                                            className="md:text-2xl text-lg text-mainBlue">{t('uploadCV')}</strong>
                                                         <MdOutlineFileUpload
-                                                            className="w-14 h-14 text-mainBlue xl:w-32 xl:h-32"/>
-                                                        <p className="text-mainBlue">{t('uploadText')}</p>
-                                                        <input type="file" hidden/>
+                                                            className="w-10 h-10 text-mainBlue xl:w-32 xl:h-32"/>
+                                                        <p className="text-mainBlue text-xs">{t('uploadText')}</p>
+                                                        <input ref={cvInputRef} type="file" onChange={handleFileChangeCv} hidden/>
+                                                        {experience && (
+                                                            <div className="flex flex-col items-center text-mainBlue mt-4 text-sm">
+                                                                {/* Отображение превью, если это изображение */}
+                                                                {experience.type.startsWith("image/") && cvPreview ? (
+                                                                    <img
+                                                                        src={cvPreview}
+                                                                        alt="Preview"
+                                                                        className="w-24 h-24 object-cover rounded-md border border-mainBlue"
+                                                                    />
+                                                                ) : null}
+
+                                                                {/* Имя файла */}
+                                                                <p className="mt-2">{experience.name}</p>
+                                                            </div>
+                                                        )}
                                                     </label>
                                                     <textarea
-                                                        className="w-full border-[#002A5F66] px-4 py-3 rounded-[10px] h-52 resize-none border"/>
+                                                        value={comment}
+                                                        className="w-full border-[#002A5F66] px-4 py-3 rounded-[10px] h-52 resize-none border"
+                                                        onChange={(e) => setComment(e.target.value)}/>
                                                 </div>
                                             </motion.div>
                                         )}
