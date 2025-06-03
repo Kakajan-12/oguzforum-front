899c2a69406f2a952b9c916282c3d83b923e1d18
Kakajan <kakajantayjikov@gmail.com>
2025-06-04 04:35:21 +0500

    change image source and secure up for api

--- next.config.ts
+++ next.config.ts
@@ -2,24 +2,24 @@ import { NextConfig } from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
 
 const nextConfig: NextConfig = {
-    // images: {
-    //     remotePatterns: [
-    //         {
-    //             protocol: 'https',
-    //             hostname: 'api.oguzforum.com',
-    //             pathname: '/uploads/**',
-    //         },
-    //     ],
-    // },
     images: {
         remotePatterns: [
             {
-                protocol: 'http',
-                hostname: 'localhost',
+                protocol: 'https',
+                hostname: 'api.oguzforum.com',
                 pathname: '/uploads/**',
             },
         ],
     },
+    // images: {
+    //     remotePatterns: [
+    //         {
+    //             protocol: 'http',
+    //             hostname: 'localhost',
+    //             pathname: '/uploads/**',
+    //         },
+    //     ],
+    // },
 };
 
 const withNextIntl = createNextIntlPlugin();
