be6a32b389d406bf0b18002c7bc6a47c17fd1bc3
Mamagul <mamagul.gapurova@mail.ru>
2026-04-04 12:07:43 +0500

    add jsdom

--- next.config.ts
+++ next.config.ts
@@ -1,27 +1,37 @@
-import { NextConfig } from 'next';
-import createNextIntlPlugin from 'next-intl/plugin';
+import { NextConfig } from "next";
+import createNextIntlPlugin from "next-intl/plugin";
 
 const nextConfig: NextConfig = {
-    images: {
-        unoptimized: true,
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'api.oguzforum.com',
-                port: '',
-                pathname: '/uploads/**',
-            },
-        ],
-    },
-    // images: {
-    //     remotePatterns: [
-    //         {
-    //             protocol: 'http',
-    //             hostname: 'localhost',
-    //             pathname: '/uploads/**',
-    //         },
-    //     ],
-    // },
+  // Потом можно удалить этот блок, если не нужно будет проксировать запросы на api.oguzforum.com
+  async rewrites() {
+    return [
+      {
+        source: "/api/oguz-proxy/:path*",
+        destination: "https://api.oguzforum.com/api/:path*",
+      },
+    ];
+  },
+  //////////////////////////////////////////////////////////////оло
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "api.oguzforum.com",
+        port: "",
+        pathname: "/uploads/**",
+      },
+    ],
+  },
+  // images: {
+  //     remotePatterns: [
+  //         {
+  //             protocol: 'http',
+  //             hostname: 'localhost',
+  //             pathname: '/uploads/**',
+  //         },
+  //     ],
+  // },
 };
 
 const withNextIntl = createNextIntlPlugin();
